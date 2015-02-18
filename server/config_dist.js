c204238d8f6af077f0f7c27492bdc0d60c7f23b4
pmary <contact@pierre-mary.fr>
2015-02-18 18:37:21 +0100

    just a comm

--- server/config_dist.js
+++ server/config_dist.js
@@ -1,7 +1,8 @@
 /*
 Meteor.startup(function () {
 	// Transactional email configuration
+	// You can use MailGun service.
 	Accounts.emailTemplates.from = 'Mapker <no-reply@mapker.co>';
-	process.env.MAIL_URL = 'theMailURL';
+	process.env.MAIL_URL = 'smtp://postmaster%40meteorize.mailgun.org:YOURPASSWORD@smtp.mailgun.org:587';
 });
 */
