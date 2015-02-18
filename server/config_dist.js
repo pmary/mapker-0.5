5df64f8b896602e1d84ab6b5609e1164202a63ae
pmary <contact@pierre-mary.fr>
2015-02-18 17:20:08 +0100

    Add config file and a gitignore to prevent to commit the production config

---
+++ server/config_dist.js
@@ -0,0 +1,7 @@
+/*
+Meteor.startup(function () {
+	// Transactional email configuration
+	Accounts.emailTemplates.from = 'Mapker <no-reply@mapker.co>';
+	process.env.MAIL_URL = 'theMailURL';
+});
+*/
