f55bf8004f2fc7bf7e0aeffc676695454f85b216
pmary <contact@pierre-mary.fr>
2015-02-18 12:30:02 -0800

    add email package

--- routes/main_routes.js
+++ routes/main_routes.js
@@ -2,35 +2,6 @@
  * Doc: https://github.com/iron-meteor/iron-router/blob/devel/Guide.md
  */
 
-Router.route('/user/:id', function () {
-  this.layout('UserProfileLayout');
-
-  // render the UserProfileBio template into the yield region named "content" 
-  // {{> yield "content"}}
-  this.render('UserProfileBio', {
-  	to: 'content',
-  	//data: function () { return Posts.findOne({_id: this.params._id})
-  });
-});
-
-Router.route('/user/:id/bio', function () {
-  this.layout('UserProfileLayout');
-
-  // render the UserProfileBio template into the yield region named "content" 
-  // {{> yield "content"}}
-  this.render('UserProfileBio', {
-  	to: 'content',
-  	//data: function () { return Posts.findOne({_id: this.params._id})
-  });
-});
-
-Router.route('/user/:id/skills', function () {
-  this.layout('UserProfileLayout');
-
-  // render the UserProfileSkills template into the yield region named "content" 
-  // {{> yield "content"}}
-  this.render('UserProfileSkills', {
-  	to: 'content',
-  	//data: function () { return Posts.findOne({_id: this.params._id})
-  });
+Router.route('/', function () {
+ 	this.render('Home');
 });
--- server/config_dist.js
+++ server/config_dist.js
@@ -2,6 +2,8 @@
 Meteor.startup(function () {
 	// Transactional email configuration
 	// You can use MailGun service.
+	// See http://blog.ploki.info/send-email-with-meteor-mailgun/ 
+	// And http://docs.meteor.com/#/full/email
 	Accounts.emailTemplates.from = 'Mapker <no-reply@mapker.co>';
 	process.env.MAIL_URL = 'smtp://postmaster%40meteorize.mailgun.org:YOURPASSWORD@smtp.mailgun.org:587';
 });
