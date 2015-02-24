957a5b524f62c03a9d5b4846a177c458a0e31ac1
pmary <contact@pierre-mary.fr>
2015-02-24 14:35:10 -0800

    Start user account related code

---
+++ client/views/user_account/user_account.js
@@ -0,0 +1,60 @@
+Template.UserLogin.events({
+	'submit #login-form' : function(e, t){
+		e.preventDefault();
+
+		// Form validation
+		var email = t.find('#login-email'),
+		password = t.find('#login-password');
+
+		var validate = fieldsValidation([
+			{
+				node: email, 
+				type: "email",
+				required:true,
+				message: "Required Field"
+			},
+			{
+				node: password, 
+				type: "password",
+				required:true,
+				message: "Required Field"
+			}
+		]);
+
+		if (validate) {
+			// Log the user in
+			Meteor.loginWithPassword(email.value, password.value, function(err){
+        		if (err){
+        			// Display the related error message
+        			console.log(err);
+        		}else {
+        			// The user has been logged in
+        		}
+        	});
+		};
+
+		return false;
+	}
+});
+
+fieldsValidation = function(fields) {
+	var status = true;
+	for (var i = fields.length - 1; i >= 0; i--) {
+		// Reinitialize all the fields error
+		fields[i].node.className = fields[i].node.className.replace(" error", "");
+		fields[i].node.parentNode.className = fields[i].node.parentNode.className.replace(" error", "");
+		fields[i].node.parentNode.parentNode.getElementsByTagName("small")[0].style.display = "none";
+
+		// If the field is required but the node is empty
+		if (fields[i].required && !fields[i].node.value) {
+			status = false;
+
+			fields[i].node.className = fields[i].node.className + " error";
+			fields[i].node.parentNode.className = fields[i].node.parentNode.className + " error";
+			fields[i].node.parentNode.parentNode.getElementsByTagName("small")[0].style.display = "block";
+			fields[i].node.parentNode.parentNode.getElementsByTagName("small")[0].innerHTML = fields[i].message;
+		};
+	};
+
+	return status;
+}
---
+++ routes/user_account.js
@@ -0,0 +1,7 @@
+/**
+ * Doc: https://github.com/iron-meteor/iron-router/blob/devel/Guide.md
+ */
+
+ Router.route('/login', function () {
+ 	this.render('UserLogin');
+});
