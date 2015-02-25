7a6151c1726b2d4b820aeba17c914c7d32eb4b16
pmary <contact@pierre-mary.fr>
2015-02-25 13:30:44 -0800

    Add the signup page and it's validation

--- client/views/user_account/user_account.js
+++ client/views/user_account/user_account.js
@@ -15,12 +15,13 @@ Template.UserLogin.events({
 			},
 			{
 				node: password, 
-				type: "password",
+				type: "password-confirmation",
 				required: true,
 				message: "Required Field"
 			}
 		]);
 
+		// If the form is valide
 		if (validate) {
 			// Log the user in
 			Meteor.loginWithPassword(email.value, password.value, function(err){
@@ -32,6 +33,68 @@ Template.UserLogin.events({
         		}
         	});
 		};
+
+		return false;
+	}
+});
+
+Template.UserJoin.events({
+	'submit #join-form' : function(e, t){
+		e.preventDefault();
+
+		// Form validation
+		var firstname = t.find('#join-first-name'),
+		lastname = t.find('#join-last-name'),
+		email = t.find('#join-email'),
+		password = t.find('#join-password'),
+		confirmedPassword = t.find('#join-confirm-password');
+
+		var validate = fieldsValidation([
+			{
+				node: firstname,
+				type: "text",
+				required: true,
+				message: "Required Field"
+			},
+			{
+				node: lastname,
+				type: "text",
+				required: true,
+				message: "Required Field"
+			},
+			{
+				node: email,
+				type: "email",
+				required: true,
+				message: "Required Field"
+			},
+			{
+				node: password,
+				type: "password",
+				required: true,
+				message: "Required Field"
+			},
+			{
+				node: confirmedPassword,
+				type: "password",
+				required: true,
+				message: "Required Field",
+				mustBeEqualTo: password
+			}
+		]);
+
+		// If the form is valide
+		if (validate) {
+			Accounts.createUser({email: email.value, password: password.value, profile:{firstname: firstname.value, lastname: lastname.value}}, function(err) {
+				if (err) {
+					// Inform the user that account creation failed
+				}else {
+					// Success. Account has been created and the user
+					// has logged in successfully. 
+					console.log("sucess account creation");
+				}
+			});
+		}
 
 		return false;
 	}
--- routes/user_account.js
+++ routes/user_account.js
@@ -2,6 +2,10 @@
  * Doc: https://github.com/iron-meteor/iron-router/blob/devel/Guide.md
  */
 
- Router.route('/login', function () {
+Router.route('/login', function () {
  	this.render('UserLogin');
+});
+
+Router.route('/join', function () {
+ 	this.render('UserJoin');
 });
