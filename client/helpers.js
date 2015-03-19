774a1125861b662cd657811500d20389232b1171
pmary <contact@pierre-mary.fr>
2015-03-19 15:48:02 +0100

    End of new form field validation system and front error management. Integrate it to the sign up form. Need to refactorize the validation functions and to implement the system to all the other forms

--- client/helpers.js
+++ client/helpers.js
@@ -5,6 +5,59 @@
  */
 uploader = new Slingshot.Upload("myFileUploads");
 
+nameValidation = function(value) {
+	if (!value) {
+		return "Required field";
+	} else if (value.length < 2) {
+		return "Ce champ doit contenir au moins 2 caractères";
+	} else if (value.length > 50) {
+		return "Ce champ ne doit pas comporter plus de 50 caractères";
+	} else if (value.search(/\d/) != -1) {
+		return "Ce champ ne doit pas contenir de chiffre";
+	} else if (value.search(/[a-zA-Z]/) == -1) {
+		return "Ce champ doit contenir au moins une lettre";
+	} else if (value.search(/[\!\@\#\$\%\^\&\*\(\)\_\+]/) != -1) {
+		return "Caractère invalide dans le nom d'utilisateur";
+	}else {
+		return false;
+	}
+}
+
+emailValidation = function(value) {
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (!re.test(value)) {
+		return "Email invalide"
+	} else {
+		return false;
+	}
+}
+
+passwordValidation = function(value) {
+	if (!value) {
+		return "Required field";
+	} else if (value.length < 6) {
+		return "Votre mot de passe doit contenir au moins 6 caractères";
+	} else if (value.length > 50) {
+		return "Votre mot de passe ne doit pas comporter plus de 50 caractères";
+	} else if (value.search(/\d/) == -1) {
+		return "Votre mot de passe doit contenir au moins un chiffre";
+	} else if (value.search(/[a-zA-Z]/) == -1) {
+		return "Votre mot de passe doit contenir au moins une lettre";
+	/*} else if (fields[i].node.value.search(/[^a-zA-Z0-9\!\@\#\$\%\^\&\*\(\)\_\+]/) != -1) {
+		isValidePassword = false;
+		passErrorMsg = "bad_char";*/
+	} else {
+		return false;
+	}
+}
+
+matchingValidation = function(value1, value2) {
+	if (value1 && value2 && value1 != value2) {
+		return "Password and confirmation doesn't match";
+	} else {
+		return false;
+	};
+}
 
 /**
  * Valadation of form fields
--- client/views/user_account/user_join.js
+++ client/views/user_account/user_join.js
@@ -1,6 +1,7 @@
 Template.userJoin.created = function() {
   Session.set('userJoinErrors', {});
 }
+
 Template.userJoin.helpers({
   errorMessage: function(field) {
     return Session.get('userJoinErrors')[field];
@@ -8,4 +9,37 @@ Template.userJoin.helpers({
   errorClass: function (field) {
     return !!Session.get('userJoinErrors')[field] ? 'has-error' : '';
   }
+});
+
+Template.userJoin.events({
+	'submit #join-form' : function(e, t){
+		e.preventDefault();
+
+		var user = {
+			firstname: t.find('#join-first-name').value,
+			lastname: t.find('#join-last-name').value,
+			email: t.find('#join-email').value,
+			password: t.find('#join-password').value,
+			passwordConfirmation: t.find('#join-confirm-password').value
+		}
+
+		var errors = validateUserJoin(user);
+		Session.set('userJoinErrors', errors);
+		if (Object.keys(errors).length)
+			return; // Abort the account creation due to errors
+
+		// If the form is valide
+		Accounts.createUser({email: user.email, password: user.password, profile:{firstname: user.firstname, lastname: user.lastname}}, function(error) {
+			if (error) {
+				// Inform the user that account creation failed
+				Errors.throw(error.reason);
+			}else {
+				// Success. Account has been created and the user
+				// has logged in successfully. 
+				console.log("sucess account creation");
+			}
+		});
+
+		return false;
+	}
 });
