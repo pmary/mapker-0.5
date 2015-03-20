da25b00790495ed09e6f6a94a03d500b729ffd79
pmary <contact@pierre-mary.fr>
2015-03-20 01:45:40 +0100

    Add geocoding and static map generation system for the 'add place' form

--- client/views/modal/modal.js
+++ client/views/modal/modal.js
@@ -4,65 +4,44 @@ Template.modal.helpers({
 	}
 });
 
+Template.modalAddPlace.created = function() {
+	Session.set('staticMapUrl', "");
+}
+
+Template.modalAddPlace.helpers({
+	staticMapUrl: function() {
+		return Session.get('staticMapUrl');
+	}
+});
+
 Template.modalAddPlace.events({
 	'click #submit-place' : function(e, t){
-		var name = t.find('#input-name'),
+		var name = t.find('#input-name').value,
 		themes = $('#input-themes').tagsinput('items'),
 		activities = $('#input-activities').tagsinput('items'),
 		role = t.find('#input-role'),
-		streetNumber = t.find('#input-street-number'),
-		streetName = t.find('#input-street-name'),
-		city = t.find('#input-city');
+		streetNumber = t.find('#input-street-number').value,
+		streetName = t.find('#input-street-name').value,
+		country = t.find('#select-country').value,
+		city = t.find('#input-city').value;
 
-		var validate = fieldsValidation([
-			{
-				node: name, 
-				type: "text",
-				required: true,
-				message: "Required Field"
-			},
-			{
-				node: themes, 
-				type: "text",
-				required: true,
-				message: "Required Field"
-			},
-			{
-				node: activities, 
-				type: "text",
-				required: true,
-				message: "Required Field"
-			},
-			{
-				node: role, 
-				type: "text",
-				required: true,
-				message: "Required Field"
-			},
-			{
-				node: streetNumber, 
-				type: "text",
-				required: true,
-				message: "Required Field"
-			},
-			{
-				node: streetName, 
-				type: "text",
-				required: true,
-				message: "Required Field"
-			},
-			{
-				node: city, 
-				type: "text",
-				required: true,
-				message: "Required Field"
-			}
-		]);
+		var address = streetNumber + streetName + city;
+		address = address.replace(/ /g, '+');
 
 		// If the form is valide
-		if (validate) {
-			// Geocoding. See : http://open.mapquestapi.com/geocoding
-			url = "http://open.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluur2508n0%2C7x%3Do5-9w2ngu&outFormat=json&inFormat=json&json={location:{street:%27"+streetNumber+" "+streetName+"%27,city:%27"+city+"%27,state:%27"+country+"%27},options:{thumbMaps:false,maxResults:1}}";
-		}
+
+		// Geocoding. See: https://developers.google.com/maps/documentation/geocoding/
+		var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&components=country:" + country;
+
+		// Get geocode
+		Meteor.http.get(url, function (error, result) {
+			if (!error) {
+				var data = result.data;
+				console.log(data);
+				// Get the static map. See: https://developers.google.com/maps/documentation/staticmaps
+				Session.set('staticMapUrl', "<img style='width: 100%;' alt='" + name + " map' src='https://maps.googleapis.com/maps/api/staticmap?center=" + address + "&zoom=13&size=600x300&maptype=terrain&markers=icon:http://mapker.co/images/pins/pin_place.png%7C" + data.results[0].geometry.location.lat + "," + data.results[0].geometry.location.lng + "'>");
+			}
+		});
+
 	}
 });
