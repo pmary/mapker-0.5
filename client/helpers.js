6474ca2f672251fed41c3f247417c8404b42e56a
Pierre Mary <contact@pierre-mary.fr>
2015-04-09 22:31:29 +0200

    End of user social profile management and display

--- client/helpers.js
+++ client/helpers.js
@@ -130,3 +130,17 @@ numberValidation = function(value) {
 		return false;
 	}
 }
+
+urlValidation = function(value) {
+	var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+	'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+	'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+	'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+	'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+	'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+	if(!pattern.test(value)) {
+		return "Must be a valid url";
+	} else {
+		return false;
+	}
+}
--- client/views/user_profile/user_profile_layout.js
+++ client/views/user_profile/user_profile_layout.js
@@ -2,7 +2,13 @@ Template.UserProfileLayout.rendered = function(){
 	//$('#identity-edit').popover();
 };
 
+/**
+ * @summary Set the 'modalResource' session variable with the data related to the current resource
+ * @locus Client
+ * @param {Object} [t] The current template instance object
+ */
 var setModalData = function(t) {
+	console.log(t);
 	var resource = {
 		id: t.data.user._id, // User id
 		cover: t.data.user.profile.cover,
@@ -13,16 +19,16 @@ var setModalData = function(t) {
 }
 
 Template.UserProfileLayout.events({
-	/**
-	 * Inner navigation UI
-	 */
+	/*****************************************************************************/
+	/* Inner navigation UI */
+	/*****************************************************************************/
 	'click #inner-nav a' : function(e, t){
 		$('#inner-nav li').removeClass('active');
 		e.target.parentNode.className = 'active';
 	},
-	/**
-	 * Avatar and cover update UI
-	 */
+	/*****************************************************************************/
+	/* Avatar and cover update UI */
+	/*****************************************************************************/
 	'click .upload-cover-btn' : function(e, t){
 		// Open the cover change modal
 		Session.set('modalChangeCoverErrors', {});
@@ -41,24 +47,46 @@ Template.UserProfileLayout.events({
 		// Init focuspoint
 		$('#helper-tool-container').jQueryFocuspointHelpertool();
 	},
-	/**
-	 * Identity edition UI
-	 */
-	'click #identity-edit' : function(e,t) {
-		/**
-		 * Render the identity edition template to the popover.
-		 * Be sure to always call Blaze.remove when the View is no longer needed.
-		 * Doc: http://docs.meteor.com/#/full/blaze_renderwithdata
-		 */
-		if (t.find('#user-infos .popover')) {
-			Blaze.renderWithData(Template.userProfileIdentityEdition, t.data, t.find('#user-infos .popover-content'));
+	/*****************************************************************************/
+	/* Identity edition UI */
+	/*****************************************************************************/
+	'click #identity-edit' : function(e,t) {		
+		if (t.find('#user-infos-identity .popover')) {
+			// Render the identity edition template to the popover.
+			// Be sure to always call Blaze.remove when the View is no longer needed.
+			// Doc: http://docs.meteor.com/#/full/blaze_renderwithdata
+			Blaze.renderWithData(Template.userProfileIdentityEdition, t.data, t.find('#user-infos-identity .popover-content'));
 		} else {
-			Session.set('userUpdateIdentityErrors', {}); // Clean the errors and prevent undefined session var
+			// Clean the errors and prevent undefined session var
+			Session.set('userUpdateIdentityErrors', {});
+			// Init the popover with empty content
 			$('#identity-edit').popover({html: true, content: " "});
+			// Show the popover
 			$('#identity-edit').popover('show');
-			Blaze.renderWithData(Template.userProfileIdentityEdition, t.data, t.find('#user-infos .popover-content'));
+			// Render the userProfileIdentityEdition template in the popover with the current template data
+			Blaze.renderWithData(Template.userProfileIdentityEdition, t.data, t.find('#user-infos-identity .popover-content'));
 		}
 	},
+	/*****************************************************************************/
+	/* Social profiles UI */
+	/*****************************************************************************/
+	'click #add-social-profiles, click #edit-social-profiles' : function(e,t) {
+		if (t.find('#user-infos-social-profiles .popover')) {
+			// Render the social profiles edition template to the popover.
+			// Be sure to always call Blaze.remove when the View is no longer needed.
+			// Doc: http://docs.meteor.com/#/full/blaze_renderwithdata
+			Blaze.renderWithData(Template.userUpdateSocialProfiles, t.data, t.find('#user-infos-social-profiles .popover-content'));
+		} else {
+			// Clean the errors and prevent undefined session var
+			Session.set('userUpdateSocialProfilesErrors', {});
+			// Init the popover with empty content
+			$('#social-profiles').popover({html: true, content: " "});
+			// Show the popover
+			$('#social-profiles').popover('show');
+			// Render the userUpdateSocialProfiles template in the popover with the current template data
+			Blaze.renderWithData(Template.userUpdateSocialProfiles, t.data, t.find('#user-infos-social-profiles .popover-content'));
+		}
+	}
 });
 
 Template.userProfileIdentityEdition.helpers({
@@ -89,16 +117,59 @@ Template.userProfileIdentityEdition.events({
 		var errors = validateUserIdentity(identity);
 		Session.set('userUpdateIdentityErrors', errors);
 		if (Object.keys(errors).length)
-			return; // Abort the account creation due to errors
+			// Abort the account creation due to errors
+			return; 
 
 		Meteor.call('userUpdateIdentity', identity, function(error, result) {
-			// display the error to the user and abort
+			// Display the error to the user and abort
 			if (error)
 				console.log(error);
 
 			// Destroy the view and the popover
 			Blaze.remove(t.view);
 			$('#identity-edit').popover('destroy');
-	    });
+		});
+	}
+});
+
+Template.userUpdateSocialProfiles.helpers({
+	errorMessage: function(field) {
+		return Session.get('userUpdateSocialProfilesErrors')[field];
+	},
+	errorClass: function (field) {
+		return !!Session.get('userUpdateSocialProfilesErrors')[field] ? 'has-error' : '';
+	}
+});
+
+Template.userUpdateSocialProfiles.events({
+	'click #close-social-profiles-popover': function (e, t) {
+		// Destroy the view and the popover
+		Blaze.remove(t.view);
+		$('#social-profiles').popover('destroy');
+	},
+	'submit #social-profiles-form' : function(e,t) {
+		e.preventDefault();
+		var socialProfiles = {
+			facebook: t.find('#edit-facebook').value,
+			flickr: t.find('#edit-flickr').value,
+			twitter: t.find('#edit-twitter').value,
+			website: t.find('#edit-website').value
+		};
+
+		var errors = validateUsersocialProfiles(socialProfiles);
+		Session.set('userUpdateSocialProfilesErrors', errors);
+		if (Object.keys(errors).length)
+			// Abort the account creation due to errors
+			return; 
+
+		Meteor.call('userUpdateSocialProfiles', socialProfiles, function(error, result) {
+			// Display the error to the user and abort
+			if (error)
+				console.log(error);
+
+			// Destroy the view and the popover
+			Blaze.remove(t.view);
+			$('#social-profiles').popover('destroy');
+		});
 	}
 });
