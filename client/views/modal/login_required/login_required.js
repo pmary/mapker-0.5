e738feef484370b1f89ae7ac03a513a5de3dab07
Pierre Mary <contact@pierre-mary.fr>
2015-05-21 01:37:20 +0200

    Various UI fixes

--- client/views/modal/login_required/login_required.js
+++ client/views/modal/login_required/login_required.js
@@ -5,3 +5,18 @@ Template.modalLoginRequired.rendered = function() {
 Template.modalLoginRequired.helpers({
 
 });
+
+Template.modalLoginRequired.events({
+	"click .user-actions-join-btn": function() {
+		// Redirect the user to the signup page
+		Router.go('userJoin');
+		// Close the modal
+		$('#myModal').modal('hide');
+	},
+	"click .user-actions-connect-btn": function() {
+		// Redirect the user to the signup page
+		Router.go('UserLogin');
+		// Close the modal
+		$('#myModal').modal('hide');
+	}
+});
