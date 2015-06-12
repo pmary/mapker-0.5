c010c8fc4f3fe04e6e192032b58be07387e9cd8d
Pierre Mary <contact@pierre-mary.fr>
2015-06-12 02:01:39 +0200

    Integration of the new home page design

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,4 +1,11 @@
 Template.Home.rendered = function(){
+	console.log('render home');
+	/*if ($(window).removeEventListener()) {
+		$(window).removeEventListener('scroll', scrollListener);
+	};*/
+
+	window.removeEventListener('scroll', scrollListener);
+
 	// If the account contain a reset token, redirect the user to the password reset page
 	if (Accounts._resetPasswordToken)
 		Router.go('reset-password.token', {resetToken: Accounts._resetPasswordToken});
@@ -6,7 +13,20 @@ Template.Home.rendered = function(){
 	this.autorun(function () {
 		// Set the menu UI
 	    $('#primary-navbar').addClass('home-ui');
+
+
+	    window.addEventListener('scroll', scrollListener);
 	});
+}
+
+var scrollListener = function () {
+	var scroll = $(window).scrollTop();
+
+	if (scroll >= 70) {
+		$('#primary-navbar').addClass('withBg');
+	} else {
+		$('#primary-navbar').removeClass('withBg');
+	}
 }
 
 Template.Home.events({
