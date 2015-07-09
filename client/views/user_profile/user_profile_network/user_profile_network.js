14db95baf4d1120090700695ef0c706bf7d7725b
Pierre Mary <contact@pierre-mary.fr>
2015-07-09 02:23:54 +0200

    Fix user profile network tab

--- client/views/user_profile/user_profile_network/user_profile_network.js
+++ client/views/user_profile/user_profile_network/user_profile_network.js
@@ -2,7 +2,24 @@ Template.userProfileNetwork.rendered = function(){
 };
 
 Template.userProfileNetwork.helpers({
-
+  /**
+   * @summary Get the data of the connected users
+   */
+   userNetwork: function () {
+     if (this.user) {
+       var user = this.user,
+       userNetwork = [];
+       console.log('user', user);
+       if ( user && user.profile.network && user.profile.network.users && user.profile.network.users.connected ) {
+         // Subscribe to users
+         Meteor.subscribe("users", user.profile.network.users.connected);
+         console.log('This user have a network');
+         userNetwork = Meteor.users.find({ _id: { $in: user.profile.network.users.connected } }).fetch();
+         console.log('userNetwork', userNetwork);
+       }
+       return userNetwork;
+     }
+   }
 });
 
 Template.userProfileNetwork.events({
