1881759e6869a708441c0e7f90c3a8fb923621ff
pmary <contact@pierre-mary.fr>
2015-07-31 17:22:54 +0200

    Disable the places test for the moment

--- tests/cucumber/features/mapker-places/editing_infos/editing_infos.js
+++ tests/cucumber/features/mapker-places/editing_infos/editing_infos.js
@@ -3,6 +3,7 @@ module.exports = function () {
   //  Edit the name types and specialities  //
   ////////////////////////////////////////////
   this.Given(/^I have created a place$/, function (callback) {
-    return this.server.call('cucumber/place/create');
+    //return this.server.call('cucumber/place/create');
+    callback.pending();
   });
 };
